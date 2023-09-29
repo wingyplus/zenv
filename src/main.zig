55fecb9d53451447273d3eba1fea4fdef3c773e7
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-09-30 00:12:07 +0700

    Support run command-line via zenv
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const fs = std.fs;
+const ArrayList = std.ArrayList;
+const ChildProcess = std.ChildProcess;
 const dotenv = @import("./dotenv.zig");
 
 pub fn main() !void {
@@ -7,13 +9,17 @@ pub fn main() !void {
     defer std.debug.assert(gpa.deinit() == .ok);
     var allocator = gpa.allocator();
 
-    var env = try dotenv.load(allocator, ".env");
-    defer env.deinit();
+    var env_map = try dotenv.load(allocator, ".env");
+    defer env_map.deinit();
 
-    var iter = env.iterator();
-    while (iter.next()) |entry| {
-        std.debug.print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-    }
+    var args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var result = try ChildProcess.exec(.{ .allocator = allocator, .argv = args[1..], .env_map = &env_map });
+    defer allocator.free(result.stdout);
+    defer allocator.free(result.stderr);
+
+    std.debug.print("{s}\n", .{result.stdout});
 }
 
 test {
